7abcb69fb2f4d9e1372753a9c5a454acc888188d
Zueva-kom <fysall337@gmail.com>
2026-07-06 14:29:35 +0800

    fix conflict

--- api/attendance.js
+++ api/attendance.js
@@ -73,24 +73,13 @@ module.exports = async (req, res) => {
 
     let statusAbsen = "MASUK";
 
-    if (currentHour >= 6 && currentHour < 15) {
-      if (currentHour >= 6 && currentHour < 9) {
-        statusAbsen = "IN"; // Disimpan sebagai 'IN' sesuai ENUM database
-      } else {
-        statusAbsen = "TERLAMBAT"; 
-      }
-    } else if (currentHour >= 15 && currentHour < 21) {
-      statusAbsen = "OUT"; // Disimpan sebagai 'OUT' sesuai ENUM database
-    } else {
-      statusAbsen = "DILUAR_JAM";
-    }
-
-    // Jika di luar jam operasional, tidak perlu insert ke log presensi database
-    if (statusAbsen === "DILUAR_JAM") {
-      return res.status(200).json({
-        status: "DILUAR_JAM",
-        name: namaSiswa
-      });
+    // KODE BARU (Saran untuk testing sekarang)
+    if (currentHour >= 0 && currentHour < 15) {
+      // Jam berapapun sebelum jam 3 siang akan dianggap masuk/terlambat dan TETAP DI-INSERT
+      statusAbsen = "IN"; 
+    } else if (currentHour >= 15 && currentHour < 24) {
+      // Jam berapapun setelah jam 3 siang akan dianggap pulang
+      statusAbsen = "OUT"; 
     }
 
     // 3. INSERT LOG PRESENSI KE POSTGRESQL
